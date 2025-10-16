2d465e4e6599473b8300079788cd51f9d8341565
VHasini27 <2420030613@klh.edu.in>
2025-10-16 11:10:50 +0530

    Add files via upload

---
+++ week 7/data.js
@@ -0,0 +1,7 @@
+export const books = [
+  { title: "This Is Not Your Story", author: "Savi Sharma", price: 350, availability: "in stock" },
+  { title: "The Silent Patient", author: "Alex Michaelides", price: 450, availability: "out of stock" },
+  { title: "The Twentieth Wife", author: "Indu Sundaresan", price: 400, availability: "in stock" },
+  { title: "A Thousand Splendid Suns", author: "Khaled Hosseini", price: 420, availability: "in stock" },
+  { title: "Red Flags and Rishtas", author: "Radhika Agarwal", price: 500, availability: "in stock" }
+];
